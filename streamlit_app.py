342935e018b6bef06b7c8cce301c3f5b89530d12
Kamlesh Patil <47054664+Neon-K9@users.noreply.github.com>
2021-12-11 14:36:57 +0530

    resolved indentation errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,30 @@
 import streamlit as st
 
-    st.title("IndicNLP!")
-    st.text("This is our Final Year Project,")
-    st.text("Implementing various features of NLP i.e,")
-    st.text("Natural Language Processing in Various Indic Languages.")
-    st.text("To Begin with Hindi,")
-    st.text("Here are Few Modules we have Implemented :-")
-    st.header('Part-Of-Speech, PoS-Tagging :- ')
-    pos_ip = st.text_input('Enter a Statement')
-    pos_btn = st.button("Process")
-    if pos_btn:
-        st.error("work's in progress :construction:, Come again later :smiley:")
-    
-    st.header('Question & Answering :- ')
-    text = st.text_area("Text to analyze")
-    que_ip = st.text_input('Enter the question')
-    qna_btn = st.button("Answer")
-    if qna_btn:
-        st.success("work's in progress :construction:, Come again later :smiley:")
-    
-    st.header('Grammar Correction :- ')
-    grm_ip = st.text_input('Enter the Statement')
-    grm_btn = st.button("Check Grammar")
-    if grm_btn:
-        st.write("work's in progress :construction:, Come again later :smiley:")
-    
-    st.caption('Thank you for Tuning in, Come back for more :heart:')
-    
+st.title("IndicNLP!")
+
+st.text("This is our Final Year Project,")
+st.text("Implementing various features of NLP i.e,")
+st.text("Natural Language Processing in Various Indic Languages.")
+st.text("To Begin with Hindi,")
+st.text("Here are Few Modules we have Implemented :-")
+
+st.header('Part-Of-Speech, PoS-Tagging :- ')
+pos_ip = st.text_input('Enter a Statement')
+pos_btn = st.button("Process")
+if pos_btn:
+    st.error("work's in progress :construction:, Come again later :smiley:")
+
+st.header('Question & Answering :- ')
+text = st.text_area("Text to analyze")
+que_ip = st.text_input('Enter the question')
+qna_btn = st.button("Answer")
+if qna_btn:
+    st.success("work's in progress :construction:, Come again later :smiley:")
+
+st.header('Grammar Correction :- ')
+grm_ip = st.text_input('Enter the Statement')
+grm_btn = st.button("Check Grammar")
+if grm_btn:
+    st.write("work's in progress :construction:, Come again later :smiley:")
+
+st.caption('Thank you for Tuning in, Come back for more :heart:')
