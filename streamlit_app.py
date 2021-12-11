9c2185c206f1ef3d583dbf920266a17754fdf88f
Kamlesh Patil <47054664+Neon-K9@users.noreply.github.com>
2021-12-11 13:53:20 +0530

    init app.py
    First draft of front-end app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# IndicNLP!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+This is our Final Year Project,
+Implementing various features of NLP i.e,
+Natural Language Processing in Various Indic Languages.
+To Begin with Hindi.
+Here are Few Modules we have Implemented :-
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    st.title("IndicNLP")
+    
+    st.header('Part-Of-Speech, PoS-Tagging :- ')
+    pos_ip = st.text_input('Enter a Statement')
+    pos_btn = st.button("Process")
+    if pos_btn:
+        st.error("work's in progress :construction:, Come again later :smiley:")
+    
+    st.header('Question & Answering :- ')
+    text = st.text_area("Text to analyze")
+    que_ip = st.text_input('Enter the question')
+    qna_btn = st.button("Process")
+    if qna_btn:
+        st.success("work's in progress :construction:, Come again later :smiley:")
+    
+    st.header('Grammar Correction :- ')
+    grm_ip = st.text_input('Enter a Statement')
+    grm_btn = st.button("Process")
+    if grm_btn:
+        st.write("work's in progress :construction:, Come again later :smiley:")
+    
+    st.caption('Thank you for Tuning in, Come back for more :heart:')
+    
