5c097c02273c856305643cb228eb1080ef4f745b
Kamlesh Patil <47054664+Neon-K9@users.noreply.github.com>
2021-12-18 13:28:16 +0530

    Redesign App UI as Sidebar navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,30 +8,31 @@ st.text("Natural Language Processing in Various Indic Languages.")
 st.text("To Begin with Hindi,")
 st.text("Here are Few Modules we have Implemented :-")
 
-import streamlit as st
-
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "Which Module would you like to try?",
+    ("Part-of-Speech Tagging", "Questiom & Answer", "Grammar Checking")
 )
 
-st.header('Part-Of-Speech, PoS-Tagging :- ')
-pos_ip = st.text_input('Enter a Statement')
-pos_btn = st.button("Process")
-if pos_btn:
-    st.error("work's in progress :construction:, Come again later :smiley:")
+if add_selectbox == "Part-of-Speech Tagging" :
+    st.header('Part-Of-Speech, PoS-Tagging :- ')
+    pos_ip = st.text_input('Enter a Statement')
+	pos_btn = st.button("Process")
+	if pos_btn:
+	    st.error("work's in progress :construction:, Come again later :smiley:")
 
-st.header('Question & Answering :- ')
-text = st.text_area("Text to analyze")
-que_ip = st.text_input('Enter the question')
-qna_btn = st.button("Answer")
-if qna_btn:
-    st.success("work's in progress :construction:, Come again later :smiley:")
+if add_selectbox == "Questiom & Answer" :
+	st.header('Question & Answering :- ')
+	text = st.text_area("Text to analyze")
+	que_ip = st.text_input('Enter the question')
+	qna_btn = st.button("Answer")
+	if qna_btn:
+	    st.success("work's in progress :construction:, Come again later :smiley:")
 
-st.header('Grammar Correction :- ')
-grm_ip = st.text_input('Enter the Statement')
-grm_btn = st.button("Check Grammar")
-if grm_btn:
-    st.write("work's in progress :construction:, Come again later :smiley:")
+if add_selectbox == "Grammar Checking" :
+	st.header('Grammar Correction :- ')
+	grm_ip = st.text_input('Enter the Statement')
+	grm_btn = st.button("Check Grammar")
+	if grm_btn:
+	    st.write("work's in progress :construction:, Come again later :smiley:")
 
 st.caption('Thank you for Tuning in, Come back for more :heart:')
